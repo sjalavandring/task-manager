7d92ee188def1cf69234ddb3eb601c169bfee4f9
username895 <89336140+username895@users.noreply.github.com>
2022-11-29 21:52:08 +0300

    возможность добавлять новые задачи

--- src/Components/TaskManager/TaskManager.tsx
+++ src/Components/TaskManager/TaskManager.tsx
@@ -1,31 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import warningImg from  '../../img/warning.jpg'
+import { useDrag, useDrop } from 'react-dnd';
 import { useSelector, useDispatch } from 'react-redux/es/exports'
 import type {taskListType} from './../../store/store';
+
 
 function TaskManager () {
     const dispatch = useDispatch()
     const taskListInfo = useSelector((state: taskListType[]) => state)
-    let taskList = taskListInfo.map((column: taskListType) => {
+    let [isTaskAdding, setTaskAdding] = useState<boolean>(false)
+    
+    let taskList = taskListInfo.map((column) => {
         return (
-            <div className="tasks-column">
-                <div className="tasks-list">
-                    <div className={"task-list-title " + `column-${column.status}` }>{column.status}</div>
-                    {column.tasks.map((task) => {
-                        return (
-                            <div className="task" draggable="true">{task.title}</div>
-                        )
-                    })}
+            <div className="tasks-column" >
+                <div className="tasks-list" >
+                    
+                        <div draggable="true" className={"task-list-title " + `column-${column.status}` }>{column.status}</div>
+                        <div className='task-box'>  
+                            {column.tasks.map((task) => {
+                                return (
+                                    <>
+                                        <div className="task task-container" key={task.id}>
+                                            <div className="task-row">
+                                                <div className="task-row__item">{task.id}</div>
+                                                <div className="task-row__item">{task.title}</div>
+                                                <div className="task-row__item">{task.dateOfCreate}</div>
+                                            </div>
+                                        </div>
+                                        {task.subtasks != undefined ? task.subtasks.map((subtask) => {
+                                            return (
+                                                <div className="subtask" key={subtask.id}>
+                                                    <div className="task-row">
+                                                        <div className="task-row__item">{task.id + "." + subtask.id}</div>
+                                                        <div className="task-row__item">{subtask.title}</div>
+                                                        <div className="task-row__item">{subtask.dateOfCreate}</div>
+                                                    </div>
+                                                </div>
+                                            )                                  
+                                        }) : ""}
+                                    </>
+                                )
+                            })}
+                    </div>
                 </div>
             </div>
         )
     })
 
+    function Taskform () {
+        let [taskTitle, setTaskTitle] = useState('')
+        let [taskDescription, setTaskDescription] = useState('')
+        let [taskPriority, setTaskPriority] = useState('')
+
+        function checkTargetValue(element: any) {
+            setTaskTitle((element.target.value).search(element.target.pattern) != -1 ? element.target.value : "" )
+        }
+
+        return (
+            <form className={"new-task-form " + (isTaskAdding ? "" : "inactive")}>
+                <div className="new-task-form-block">
+                    <input className="new-task-form__item" type="text" placeholder="Имя задачи (до 10 символов)" pattern="[A-Za-zА-Яа-яЁё]{1,10}" onChange={(e) => console.log(e.target.pattern)} required/>
+                    <input className="new-task-form__item" type="text" placeholder="Приоритет (от 1 до 3, где 1 - наибольший приоритет)" pattern="[1-3]" required/>
+                </div>
+                <div className="new-task-form-block">
+                    <input className="new-task-form__item new-task-form_textarea" type="textarea" placeholder="Имя задачи (до 100 символов)" pattern="[A-Za-zА-Яа-яЁё]{,100}"/>
+                </div>
+                <div className="new-task-form-block">
+                    <button className='new-task-form__item submit_button' type="submit" onClick={() => {dispatch({type: "add_task", description: 'testdesc', title: "testtitle", priority: 2})}}>Создать задачу</button>
+                </div>  
+            </form> 
+        )
+    }
+
     return (
-        <div className="task-manager wrapper">
-            <div className="task-manager-columns">
-                {taskList}
+        <>
+            <button className="new-task-add" onClick={() => {setTaskAdding(true)}}>Добавить задачу</button>
+            {isTaskAdding  ? <Taskform/> : null}
+            <div className={isTaskAdding ? "shadowBack" : ""} onClick={() => {setTaskAdding(false)}}></div>
+            <div className="task-manager wrapper">
+                <div className="task-manager-columns">
+                    {taskList}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,22 +1,73 @@
 import {combineReducers, createStore} from 'redux'
 
+type CommentType = {
+    text: string,
+    subcomments: CommentType[],
+}
+
 type taskListType = {
     id: number,
     status: string,
     tasks: {
         id: number,
-        title: string
+        type: string,
+        title: string,
+        dateOfCreate: string,
+        dateOfDone?: string,
+        timeInDev?: string,
+        description: string,
+        priority: number,
+        comments?: CommentType,
+        subtasks?: {
+            id: number,
+            type: string,
+            title: string,
+            dateOfCreate: string,
+            dateOfDone?: string,
+            timeInDev?: string,
+            description: string,
+            priority: number,
+        }[],
+
     }[]
 }
 
 let taskListInfo: taskListType[] = [
-    {id: 1, status: "Queue", tasks: [{id: 1, title: "Task1"}, {id: 2, title: "Task2"}, {id: 3, title: "Task3"}]},
-    {id: 2, status: "Development", tasks: [{id: 4, title: "Task4"}, {id: 5, title: "Task5"}, {id: 6, title: "Task6"}]},
-    {id: 3, status: "Done", tasks: [{id: 7, title: "Task7"}, {id: 8, title: "Task8"}, {id: 9, title: "Task9"}]}
+    {id: 1, status: "Queue", tasks: [
+        {
+            id: 1, 
+            title: "Task1", 
+            dateOfCreate: (new Date()).toISOString().slice(0,10),
+            type: "task",
+            description: "test description, test description, test description",
+            priority: 1,
+            subtasks: [{
+                id: 1,
+                type: "subtask",
+                title: "subtask",
+                dateOfCreate: (new Date()).toISOString().slice(0,10),
+                description: "test",
+                priority: 1,
+            }]
+        }]},
+    {id: 2, status: "Development", tasks: []},
+    {id: 3, status: "Done", tasks: []}
 ]
 
 let taskInfoReducer = (state = taskListInfo, action: any) => {
     switch (action.type) {
+        case "add_task" :
+            let newTasks = taskListInfo[0].tasks;
+            newTasks.push({
+                id: taskListInfo[0].tasks.length + 1, 
+                type: "task",
+                title: action.title, 
+                dateOfCreate: (new Date()).toISOString().slice(0,10),
+                description: action.description,
+                priority: action.priority,
+            })
+            console.log({...state, tasks: newTasks})
+            return [{...state[0], tasks: newTasks}, {...state[1]}, {...state[2]}]
         default: 
             return state
     }
